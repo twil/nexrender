6c2320f1cc0ed4219a40a055e6051209a88e89f3
U-new-chaos\Twil <ak@gocream.ru>
2018-10-06 01:51:31 +0300

    Download with cache - initial commit

--- renderer/tasks/download.js
+++ renderer/tasks/download.js
@@ -3,10 +3,14 @@
 let logger     = require('../logger');
 
 const download = require('download');
+const mkdirp   = require('mkdirp');
 const fs       = require('fs-extra');
 const path     = require('path');
 const AWS      = require('aws-sdk');
 const url      = require('url');
+const crypto   = require('crypto');
+
+const CACHE_DIR = process.env.CACHE_DIR || 'cache';
 
 
 function isLocalPath(src) {
@@ -20,6 +24,73 @@ function copy(src, dstDir) {
             return (err ? reject(err) : resolve());
         });
     });
+}
+
+/**
+ * Check for asset.md5 hash and if present - save to a cache
+ */
+function downloadWithCache(asset, dst) {
+    let cacheDst = path.join(CACHE_DIR, dst);
+    let fileName = path.basename(url.parse(asset.src).pathname);
+    let cacheFile = path.join(cacheDst, fileName);
+
+    // download
+    let downloadFunc = function(resolve, reject) {
+        download(asset.src, cacheDst, {
+            retry: 3//,
+            //timeout: 120 * 1000 // 2 minutes
+        })
+        .then(() => {
+            return copy(cacheFile, dst);
+        })
+        .then(() => {
+            return resolve();
+        })
+        .catch((err) => {
+            return reject(err);
+        });
+    };
+
+    // download
+    if(typeof asset.md5 === 'undefined' || !fs.existsSync(cacheFile)) {
+        return new Promise((resolve, reject) => {
+            downloadFunc(resolve, reject);
+        });
+    }
+    // check cache
+    else {
+        return new Promise((resolve, reject) => {
+            let hash = crypto.createHash('md5');
+            let stream = fs.createReadStream(cacheFile);
+
+            stream.on('data', function(data) {
+                hash.update(data, 'utf8');
+            })
+
+            stream.on('end', function() {
+                let h = hash.digest('hex');
+
+                // hit
+                if(asset.md5 == h) {
+                    copy(cacheFile, dst)
+                    .then(() => {
+                        return resolve();
+                    })
+                    .catch((err) => {
+                        return reject(err);
+                    });
+                }
+                // not hit
+                else {
+                    downloadFunc(resolve, reject);
+                }
+            })
+
+            stream.on('error', function(error) {
+                downloadFunc(resolve, reject);
+            })
+        });
+    }
 }
 
 function downloadFromS3(bucket, key, dstDir, dstName) {
@@ -44,6 +115,8 @@ module.exports = function(project) {
 
         logger.info(`[${project.uid}] downloading assets...`);
 
+        mkdirp.sync(CACHE_DIR);
+
         // iterate over each asset to check for custom template
         for (let asset of project.assets) {
             // check for custom template
@@ -57,10 +130,12 @@ module.exports = function(project) {
             if (asset.type === 's3') {
                 return downloadFromS3(asset.bucket, asset.key, project.workpath, path.basename(url.parse(asset.src).pathname));
             } else if (asset.type === 'url' || !isLocalPath(asset.src)) {
-                return download(asset.src, project.workpath, {
-                    retry: 3//,
-                    //timeout: 120 * 1000 // 2 minutes
-                });
+                // return download(asset.src, project.workpath, {
+                //     retry: 3//,
+                //     //timeout: 120 * 1000 // 2 minutes
+                // });
+
+                return downloadWithCache(asset, project.workpath);
             } else if (asset.type === 'path' || isLocalPath(asset.src)) {
                 return copy(asset.src, project.workpath);
             }
