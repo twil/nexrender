90a88f90b92b37c8ff32e52c26aa5685afb66403
Alexander Kaidalov <twildor@gmail.com>
2018-10-22 18:29:02 +0300

    make a copy of a command

--- renderer/tasks/ffmpeg.js
+++ renderer/tasks/ffmpeg.js
@@ -49,15 +49,19 @@ module.exports = function(project) {
         */
 
         async.eachSeries(project.ffmpeg, function(rule, callback) {
-            rule.unshift('-y');
-            for(let i = 0; i < rule.length; i++) {
-                rule[i] = rule[i].replace('%SOURCE%', source)
-                                 .replace('%RESULT%', destination)
-                                 .replace('%WORKPATH%', project.workpath);
+            // make a copy
+            let command = JSON.stringify(rule);
+            command = JSON.parse(command);
+            command.unshift('-y');
+
+            for(let i = 0; i < command.length; i++) {
+                command[i] = command[i].replace('%SOURCE%', source)
+                                       .replace('%RESULT%', destination)
+                                       .replace('%WORKPATH%', project.workpath);
             }
 
             // spawn process and begin rendering
-            let ffmpeg = spawn(FFMPEG, rule);
+            let ffmpeg = spawn(FFMPEG, command);
 
             let ffmpegdata = [];
 
