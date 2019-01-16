ef93ff92e2eac6a1c2f2233b0116c7ddd2adae58
U-new-chaos\Twil <ak@gocream.ru>
2019-01-16 19:59:49 +0300

    Set log level through an env variable AE_LOG_LEVEL

--- renderer/logger.js
+++ renderer/logger.js
@@ -4,6 +4,7 @@ var winston = require('winston');
 
 
 var logger = new (winston.Logger)({
+    level: process.env.AE_LOG_LEVEL || 'info',
     transports: [
         new (winston.transports.Console)({ timestamp: true })
     ]
